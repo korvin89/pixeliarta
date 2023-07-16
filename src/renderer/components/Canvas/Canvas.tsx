44d2813270b3cad3160d41a0b64b014147308b95
Alaev Evgeny <alaev89@yandex.ru>
2023-07-16 12:35:35 +0200

    feat: add Canvas component

---
+++ src/renderer/components/Canvas/Canvas.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {CanvasItem} from '../CanvasItem/CanvasItem';
+
+import {useStyle} from './useStyle';
+
+export const Canvas = () => {
+    const ref = React.useRef<HTMLDivElement>(null);
+    const [rect, setRect] = React.useState<DOMRect | undefined>();
+    const style = useStyle();
+
+    // нужно прикапывать позицию курсора в redux
+    // брать позицию только тогда, когда нужно
+
+    React.useEffect(() => {
+        setRect(ref.current?.getBoundingClientRect());
+    }, []);
+
+    return (
+        <div ref={ref} style={style}>
+            {rect?.width && rect?.height && <CanvasItem width={rect.width} height={rect.height} />}
+        </div>
+    );
+};
---
+++ src/renderer/components/Canvas/useStyle.ts
@@ -0,0 +1,8 @@
+import type {UseStyle} from '../types';
+
+export const useStyle: UseStyle = () => {
+    return {
+        width: 100,
+        height: 100,
+    };
+};
